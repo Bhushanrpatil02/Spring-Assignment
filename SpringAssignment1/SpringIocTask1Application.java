a3132dfaea67a5432102510d3864a76fdfbf10b5
Bhushanrpatil02 <106161731+Bhushanrpatil02@users.noreply.github.com>
2022-09-15 16:34:17 +0530

    Add files via upload

---
+++ SpringAssignment1/SpringIocTask1Application.java
@@ -0,0 +1,13 @@
+package com.yash.SpringAssignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocTask1Application.class, args);
+	}
+
+}
